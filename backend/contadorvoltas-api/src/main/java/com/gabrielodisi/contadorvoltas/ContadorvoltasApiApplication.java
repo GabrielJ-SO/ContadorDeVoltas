04a84e62c26b7b28f9f3b61d65b32c8e701c5eb3
GabrielJ-SO <gabrielodisi20@gmail.com>
2026-01-12 20:08:42 -0300

    inicio da api

---
+++ backend/contadorvoltas-api/src/main/java/com/gabrielodisi/contadorvoltas/ContadorvoltasApiApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielodisi.contadorvoltas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContadorvoltasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContadorvoltasApiApplication.class, args);
+	}
+
+}
---
+++ backend/contadorvoltas-api/src/test/java/com/gabrielodisi/contadorvoltas/ContadorvoltasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielodisi.contadorvoltas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContadorvoltasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
